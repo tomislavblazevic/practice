83fab2f4590fd4eb6a8ef400fc85b2cf2fae790d
Tomislav Blazevic <tekennster@gmail.com>
2023-06-09 00:07:10 +0200

    added JS files and greeting

---
+++ myScript1.js
@@ -0,0 +1,3 @@
+
+const d = new Date();
+document.getElementById("time").innerHTML = d;
---
+++ myScript2.js
@@ -0,0 +1,11 @@
+
+const hour = new Date().getHours(); 
+let greeting;
+
+if (hour < 18) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+
+document.getElementById("good day-evening").innerHTML = greeting;
